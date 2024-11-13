f76b14a893bb5b4ac4e597b6112a6544ad71bbe4
Pedro Josue <pedro.josue.ap@outlook.com>
2024-11-13 15:50:33 -0300

    configurando as rotas na main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import App from './App.jsx'
+import Cadastrar from './routes/Cadastrar.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Login from './routes/Login.jsx'
+import Sobre from './routes/Sobre.jsx'
+import Solucao from './routes/Solucao.jsx'
 
+const router =createBrowserRouter([{
+
+  //Elementos PAI
+  path:'/',element:<App/>,
+  errorElement:<Error/>,
+
+  //Elemento Filho
+  children:[
+    {path:'/',element:<Home/>},
+    {path:'/',element:<Cadastrar/>},
+    {path:'/',element:<Login/>},
+    {path:'/',element:<Sobre/>},
+    {path:'/',element:<Solucao/>},
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
