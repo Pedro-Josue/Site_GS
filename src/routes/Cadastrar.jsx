2f1f9bac0296d28d2fbab3e1141c4aa37c03f4b5
Pedro Josue <pedro.josue.ap@outlook.com>
2024-11-13 15:39:21 -0300

    arrow functions de todos os components e routes criados

---
+++ src/routes/Cadastrar.jsx
@@ -0,0 +1,9 @@
+const Cadastrar=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Cadastrar
---
+++ src/routes/Solucao.jsx
@@ -0,0 +1,9 @@
+const Solucao=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Solucao
