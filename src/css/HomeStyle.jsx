efa6e918aeed3ddb14f7d47b1b1dffd9eaa71833
Pedro Josue <pedro.josue.ap@outlook.com>
2024-11-14 15:31:05 -0300

    adicionando slideshow e titulo a home, mas com imagens aleatorias

---
+++ src/css/HomeStyle.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HomeStyle = styled.section`
+    .titulo-home{
+        display: flex;
+        justify-content: center;
+        margin-top: 2%;
+        margin-bottom: 2%;
+    }
+
+`
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,50 @@
+import { HomeStyle } from "../css/HomeStyle"
+import Carousel from 'react-bootstrap/Carousel';
+
 const Home=()=>{
     return(
-        <>
-            <h1>Olha aqui a home</h1>
-        </>
+        <HomeStyle>
+            <h2 className="titulo-home">Energia Sustentável para um Futuro Mais Limpo</h2>
+            <Carousel className="w-50 mx-auto">
+      <Carousel.Item>
+        <img
+          className="d-block w-100" // classes do Bootstrap para largura total
+          src="src\assets\energia-solar.jpg" // Caminho da primeira imagem
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100" // classes do Bootstrap para largura total
+          src="src\assets\energia-solar.jpg" // Caminho da segunda imagem
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100" // classes do Bootstrap para largura total
+          src="src\assets\energia-solar.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </HomeStyle>
     )
 }
 
