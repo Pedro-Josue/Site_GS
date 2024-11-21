72f8eb96deb675ecab33cb66624c5def982dac36
Pedro Josué <pedro.josue.ap@outlook.com>
2024-11-21 12:22:16 -0300

    Adicionando os textos da sobre, sem css

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,7 +1,26 @@
 const Sobre=()=>{
     return(
         <>
-            <h1>Sobre</h1>
+            <h2>Sobre o Projeto</h2>
+            <p>A SolarTech utiliza mini painéis solares para carregar baterias de lítio, oferecendo uma alternativa ecológica ao uso de pilhas descartáveis. Ao usar energia solar, nossa solução visa reduzir o impacto ambiental causado pelo descarte de pilhas e promover uma forma mais sustentável de alimentar dispositivos portáteis.</p>
+            <h2>Como Funciona?</h2>
+            <div>
+                <ul>
+                    <li>Painel Solar: Captura a luz solar e converte em energia elétrica.</li>
+                    <li>Armazenamento: A energia é armazenada em baterias de lítio recarregáveis.</li>
+                    <li>Uso Sustentável: A bateria recarregada alimenta seus dispositivos de maneira ecológica.</li>
+                </ul>
+            </div>
+            <h2>Benefícios para o meio ambiente</h2>
+            <div>
+                <ul>
+                    <li>Reduzimos o uso de pilhas descartáveis, evitando poluição e resíduos tóxicos.</li>
+                    <li>Energia Solar Limpa e Renovável: Utilizamos uma fonte de energia sem impacto ambiental.</li>
+                    <li>Menor Pegada de Carbono: Reduzimos emissões ao optar por energia solar.</li>
+                </ul>
+            </div>
+            <h2>Impacto e visão do futuro</h2>
+            <p>Nosso objetivo é criar uma alternativa prática e sustentável para reduzir o uso de pilhas e promover um consumo mais consciente. Ao escolher a SolarTech, você participa da construção de um futuro mais verde.</p>
         </>
     )
 }
